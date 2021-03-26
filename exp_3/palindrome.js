bcc8c6eb651b30b1012f69039d4baf4cbc5e298e
mishra5047 <mishra695047@gmail.com>
2021-03-26 13:19:19 +0530

    Exp_2 and Exp_3

---
+++ exp_3/palindrome.js
@@ -0,0 +1,16 @@
+let args = "aaaaaa"
+
+console.log(checkPal(args));
+
+
+function checkPal(string){
+    let str = 0, end = string.length - 1;
+    while(str < end){
+        if(string[str] != string[end]){
+            return false;
+        }
+        str++;
+        end--;
+    }
+    return true;
+}
